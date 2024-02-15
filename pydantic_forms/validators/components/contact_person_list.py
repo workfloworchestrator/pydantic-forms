a3dd1b4dd4bb9eba01daefa35f3d5f4028c118a8
Mark90 <mark_moes@live.nl>
2024-02-15 14:25:41 +0100

    Remove non-functioning deprecated=True settings

--- pydantic_forms/validators/components/contact_person_list.py
+++ pydantic_forms/validators/components/contact_person_list.py
@@ -10,6 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import warnings
 from typing import Annotated, Generator, Optional, TypeVar
 from uuid import UUID
 
@@ -33,12 +34,14 @@ def contact_person_list(
         if organisation_key:
             yield "organisationKey", organisation_key
 
+    warnings.warn(
+        "organisationId and organisationKey attributes will be removed, for version with organisation "
+        "(renamed to customer) use `from orchestrator.forms.validators import customer_contact_list` instead of this",
+        DeprecationWarning,
+        stacklevel=2,
+    )
     return Annotated[
         conlist(ContactPerson, min_length=min_items, max_length=max_items),
         BeforeValidator(remove_empty_items),
-        Field(
-            json_schema_extra=dict(json_schema_extra()),
-            deprecated=True,
-            description="organisationId and organisationKey attributes will be removed, for version with organisation (renamed to customer) use `from orchestrator.forms.validators import customer_contact_list` instead of this",
-        ),
+        Field(json_schema_extra=dict(json_schema_extra())),
     ]  # type: ignore
